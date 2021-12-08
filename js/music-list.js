645c0e6793d7b6fb19405de48bc1122f3e364716
dhruvi-mehta <mehtd083@newschool.edu>
2021-12-08 13:48:54 -0500

    hi

---
+++ js/music-list.js
@@ -0,0 +1,70 @@
+
+let allMusic = [
+  {
+    name: "I Hate U",
+    artist: "SZA",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "you were good to me ",
+    artist: "Jeremey Zucker, Chelsea Cutler",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Falling",
+    artist: "Harry Styles",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Better Now",
+    artist: "Oh Wonder",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Dopamine",
+    artist: "Marc Wavy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Drew Barrymore",
+    artist: "SZA",
+    img: "music-6",
+    src: "music-6"
+
+  },
+  {
+    name: "Grateful",
+    artist: "Mahalia",
+    img: "music-7",
+    src: "music-7"
+
+  },
+  {
+    name: "Cyanide",
+    artist: "Daniel Caesar",
+    img: "music-8",
+    src: "music-8"
+
+  },
+  {
+   name: "Tightrope",
+    artist: "zayn",
+    img: "music-9",
+    src: "music-9"
+
+  },
+  {
+    name: "UCLA",
+    artist: "RL Grime, 24hrs ",
+    img: "music-10",
+    src: "music-10"
+
+  }
+
+
+];
